35aec0514af63a8c21002e84c3156d39d151dd35
Tarek vonSeckendorff <tarek_vs@hotmail.com>
2023-10-08 17:10:56 +0200

    Comments update

--- emensa/controllers/HomeController.php
+++ emensa/controllers/HomeController.php
@@ -118,31 +118,117 @@ class HomeController
      */
     public function myprofile(){
 
-        // ... [the rest of the function remains unchanged]
+        if(isset($_SESSION['name']))
+        {
+            $myratings = myratings($_SESSION['name']);
 
+            if (isset($_GET['delete-rating']))
+            {
+                $user = getrating($_GET['delete-rating'])[0]['benutzer'];
+
+                if ($user == $_SESSION['name'])
+                {
+                    deleterating($_GET['delete-rating']);
+                    header("Location:http://localhost:9000/MyProfile",true,301);
+                    exit();
+                }
+            }
+        }
+        return view('myprofile',['_SESSION'=>$_SESSION, 'ratings' => $myratings]);
     }
-
     /**
      * Handles the dish rating functionalities.
      *
      * @return mixed The appropriate view based on the user's actions.
      */
-    public function bewertung(){
+    public function bewertung()
+    {
+        if (isset($_SESSION['eingeloggt']) && $_SESSION['eingeloggt'] == true)
+        {
+            if (isset($_GET['gerichtid']))
+            {
+                $gericht = select_gericht_id($_GET['gerichtid']);
+                $ratings = getratings(($_GET['gerichtid']));
 
-        // ... [the rest of the function remains unchanged]
+                if (isset($_POST['benutzer']) && isset($_POST['gerichtid']) && isset($_POST['bemerkung'])&& isset($_POST['bewertung']))
+                {
+                    addrating($_POST['benutzer'], $_POST['bemerkung'], $_POST['bewertung'],$_POST['gerichtid']);
+                    header("refresh:0");
+                }
+
+                if (isset($_GET['delete-rating']))
+                {
+                    $user = getrating($_GET['delete-rating'])[0]['benutzer'];
+
+                    if ($user == $_SESSION['name'])
+                    {
+                        deleterating($_GET['delete-rating']);
+                        header("Location:http://localhost:9000/bewertung?gerichtid=".$_GET['gerichtid'],true,301);
+                        exit();
+                    }
+                }
+
+                return view('bewertung', ['data' => $gericht, 'ratings' => $ratings]);
+            }
+
+        }
+        else
+        {
+            header("Location:http://localhost:9000/Anmelden?redirect=bewertung&gerichtid=".$_GET['gerichtid'],true,301);
+            exit();
+        }
 
     }
+    
 
     /**
      * Handles the functionalities related to all ratings.
      *
      * @return mixed The ratings view with the relevant data.
      */
-    public function bewertungen(){
-
-        // ... [the rest of the function remains unchanged]
-
-    }
+    public function bewertungen()
+        {
+            $allratings = getallratings();
+            $admin = isadmin($_SESSION['name']);
+            $isadmin = 0;
+    
+            if (isset($admin[0]['admin']) && $admin[0]['admin'] == 1)
+            {
+                $isadmin = 1;
+            }
+    
+    
+            if (isset($_GET['delete-rating']))
+            {
+                $user = getrating($_GET['delete-rating'])[0]['benutzer'];
+    
+                if ($user == $_SESSION['name'])
+                {
+                    deleterating($_GET['delete-rating']);
+                    header("Location:http://localhost:9000/bewertungen",true,301);
+                    exit();
+                }
+            }
+    
+            if (isset($_GET['mark']) && isset($_GET['rating-id']))
+            {
+                if ($_GET['mark'] == "nein")
+                {
+                    mark_rating($_GET['rating-id'],false);
+                }
+    
+                if($_GET['mark'] == "ja")
+                {
+                    mark_rating($_GET['rating-id'],true);
+                }
+    
+                header("Location:http://localhost:9000/bewertungen",true,301);
+                exit();
+            }
+    
+            return view('bewertungen', ['ratings' => $allratings, 'isadmin' => $isadmin]);
+        }
+    
 
     /**
      * Displays the opinions view with marked ratings.
